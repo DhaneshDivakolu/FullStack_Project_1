b46427616231108da410a22940f3e518686d2a0f
DhaneshDivakolu <dhaneshchowdary0@gmail.com>
2024-12-11 21:04:01 +0530

    Version email

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask,render_template,request,redirect
+from flask_mail import Mail, Message
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587 
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = "Eneter your Mail@gmail.com"
+app.config["MAIL_PASSWORD"] = "Enter your app password of your google"
+mail = Mail(app)
+
 myclient = MongoClient("localhost", 27017)
 mydb = myclient["Calci"]
 results = mydb["results"]
@@ -47,23 +55,31 @@ def cal():
             n1 = int(request.form["inp1"])
             opr = request.form["opr"]
             n2 = int(request.form["inp2"])
+            msg = Message(subject="Calculation",sender="Eneter your Mail@gmail.com", 
+                        recipients=["abc@gamil.com","def@gmail.com"])   #one or more reciever mail
             if opr=="add":
                 res = f"{n1} + {n2} is {n1+n2}"
                 results.insert_one({
                     "number1":n1, "number2":n2, "operator":opr, "output":res
                 })
+                msg.body=res
+                mail.send(msg)
                 return render_template("index.html" , output = res)
             if opr=="sub":
                 res = f"{n1} - {n2} is {n1-n2}"
                 results.insert_one({
                     "number1":n1, "number2":n2, "operator":opr, "output":res
                 })
+                msg.body=res
+                mail.send(msg)
                 return render_template("index.html" , output = res)
             if opr=="mul":
                 res = f"{n1} x {n2} is {n1*n2}"
                 results.insert_one({
                     "number1":n1, "number2":n2, "operator":opr, "output":res
                 })
+                msg.body=res
+                mail.send(msg)
                 return render_template("index.html" , output = res)
             if opr=="div":
                 try:
@@ -71,6 +87,8 @@ def cal():
                     results.insert_one({
                     "number1":n1, "number2":n2, "operator":opr, "output":res
                     })
+                    msg.body=res
+                    mail.send(msg)
                     return render_template("index.html" , output = res)
                 except ZeroDivisionError as e:
                     res = "Please change num2 as Non-Zero"
